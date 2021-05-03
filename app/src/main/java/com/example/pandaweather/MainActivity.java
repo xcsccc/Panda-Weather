67ec495c85b7365f6328137c1fbd6f1bfe167403
xcs <540370483@qq.com>
2021-05-03 22:57:55 +0800

    Second commit.

--- app/src/main/java/com/example/pandaweather/MainActivity.java
+++ app/src/main/java/com/example/pandaweather/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pandaweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            v.animate().translationX(-100).getDuration();
+        });
     }
 }
